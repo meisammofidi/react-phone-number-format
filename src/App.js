93b9146ed7ee359916a2d3b4b0aad3ccbec8421d
Meisam Mofidi <meisam.mofidi@telus.com>
2023-03-11 15:51:52 -0500

    add format to phone number

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
   return (
-  <div>
-    Format Phone Numberr
-  </div>
-
+    <>
+      <div className="contain">
+        <h1>Phone Number</h1>
+        <PhoneNumberInput />
+      </div>
+    </>
   );
 }
+
+const PhoneNumberInput = () => {
+  const [userInput, setUserInput] = useState("");
+  const handleInput = (e) => {
+    const formatedNumber = formatNumberPhone(e.target.value);
+    setUserInput(formatedNumber);
+  };
+  const formatNumberPhone = (number) => {
+    if(!number) return number
+    const phoneNumber = number.replace(/[^\d]/g, "");
+    return `(${phoneNumber.slice(0,3)}) ${phoneNumber.slice(3,6)}-${phoneNumber.slice(6,10)}`
+  };
+  return (
+    <>
+      <input className="space" onChange={(e) => handleInput(e)} value={userInput} />
+      <div className="display">Phone: {userInput}</div>
+    </>
+  );
+};
 
 export default App;
